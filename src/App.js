f65dd43537ccad5c3c591a64e42ff2e81fe6ccf9
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2025-09-24 12:19:50 -0400

    created current weather layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      Weather App{" "}
-      <footer>
-        {" "}
-        This project was coded by Jennifer Abreu and is open-sourced on{" "}
-        <a
-          href="https://github.com/Jabreu0715/react-weather-app-2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          {" "}
+          This project was coded by Jennifer Abreu and is open-sourced on{" "}
+          <a
+            href="https://github.com/Jabreu0715/react-weather-app-2"
+            target="_blank"
+            rel="noreferrer"
+            className="footer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-8">
+            <input
+              type="search"
+              placeholder="Enter a City..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-4">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-light w-75"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York City</h1>
+      <ul>
+        <li>Wednesday 11:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://www.gstatic.com/weather/conditions2023/2023.2/svg/mostly_cloudy_day_light.svg"
+            alt="Mostly Cloudy"
+            className="img-fluid"
+          />
+          <span className="current-temp">20</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 50%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
