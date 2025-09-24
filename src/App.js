9ffe5af2e1709cfbec03c24c306319abfdc27294
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2025-09-24 09:59:30 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App{" "}
+      <footer>
+        {" "}
+        This project was coded by Jennifer Abreu and is open-sourced on{" "}
+        <a
+          href="https://github.com/Jabreu0715/react-weather-app-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
