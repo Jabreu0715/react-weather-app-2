e282f5c7cd0d618ff5bab3e734208ccdcad590a4
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2025-09-29 12:48:40 -0400

    Finished formatting the current temperature and unit

--- src/Weather.js
+++ src/Weather.js
@@ -27,22 +27,24 @@ export default function Weather() {
         <li>Wednesday 11:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://www.gstatic.com/weather/conditions2023/2023.2/svg/mostly_cloudy_day_light.svg"
-            alt="Mostly Cloudy"
-            className="img-fluid"
-          />
-          <span className="current-temp">20</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 50%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
+      <div className="current-weather">
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://www.gstatic.com/weather/conditions2023/2023.2/svg/mostly_cloudy_day_light.svg"
+              alt="Mostly Cloudy"
+              className="img-fluid"
+            />
+            <span className="current-temp">20</span>
+            <span className="unit">°C</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 50%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
