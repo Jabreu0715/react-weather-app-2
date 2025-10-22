b684341013ad9113a043736825bbd7a0547144eb
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2025-10-22 12:22:42 -0400

    added links to my footer

--- src/App.js
+++ src/App.js
@@ -8,15 +8,36 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>
           {" "}
-          This project was coded by Jennifer Abreu and is open-sourced on{" "}
+          This project was coded by{" "}
           <a
-            href="https://github.com/Jabreu0715/react-weather-app-2"
+            href="https://github.com/Jabreu0715"
             target="_blank"
             rel="noreferrer"
             className="footer"
           >
-            GitHub
-          </a>
+            {" "}
+            Jennifer Abreu
+          </a>{" "}
+          and is open-sourced on{" "}
+          <span>
+            <a
+              href="https://github.com/Jabreu0715/react-weather-app-2"
+              target="_blank"
+              rel="noreferrer"
+              className="footer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://taupe-meerkat-721582.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="footer"
+            >
+              Netlify
+            </a>
+          </span>
         </footer>
       </div>
     </div>
