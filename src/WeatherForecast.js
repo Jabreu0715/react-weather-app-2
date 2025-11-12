a232d7451f521c4bce544d4cc1baa360cdb074d7
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2025-11-12 15:15:06 -0500

    created the WeatherForecastDay component with functioning date, temperatures and icon using props.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,28 +1,37 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
     console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
   console.log(props);
-  const apiKey = `23ftbd4aecd5fa6f4304ea2800dofdbf`;
-  let city = props.city;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="clear-sky-day" size={36} />{" "}
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19° </span>|{" "}
-            <span className="WeatherForecast-temperature-min">10°</span>
+
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            {" "}
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `23ftbd4aecd5fa6f4304ea2800dofdbf`;
+    let city = props.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
